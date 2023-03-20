98924c5d759cff2f526241c0c43dd8a093d712c4
Bunmi <ifeimie@yahoo.com>
2023-03-20 22:32:25 +0000

    milestone_2 alphaval

--- milestone_2.py
+++ milestone_2.py
@@ -5,13 +5,15 @@
 # Step 1. Go to the first line of your file.
 
 # Step 2. Write import random on the line. Note: To import a module, you have to use the import keyword at the top of the file.
-
+#%%
 import random
 word_list = ["mango", "apple", "guava", "orange", "pear"]
 print(word_list)
 # pick a random choice from a list of strings.
 word = random.choice(word_list)
 print(word)
+
+#%%
 # Step 1. Using the input function, ask the user to enter a single letter.
 
 # Step 2. Assign the input to a variable called guess.
@@ -19,3 +21,12 @@ print("Enter a single letter")
 guess = input()
 print(guess)
 
+#%%
+# Step 1. Create an if statement that checks if the length of the input is equal to 1 and the input is alphabetical.
+# Step 2: In the body of the if statement, print a message that says "Good guess!".
+# Step 3: Create an else block that prints "Oops! That is not a valid input." if the preceeding conditions are not met.
+
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
+else:
+    print ('Oops! That is not a valid input.')
